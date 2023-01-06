357db2f430d5f476a1293c6f26461bcaa894323c
Elias Abril-Fernandez <50235043+elias-abril-f@users.noreply.github.com>
2023-01-06 15:09:04 +0000

    Deploy settings updated

--- Crawler/Crawler/settings.py
+++ Crawler/Crawler/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k9p6u9ip5vo1+8l=c0@@m0699o+wobc6xux2ce**-np&xnivk@'
+SECRET_KEY = 'k9p6u9ip5vo1+8l=c0@@m0699o+wob&^$*)(*c6xux2ce**-np&xnivk@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,3 +114,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Things that i've changed  
+
+SECURE_HSTS_SECONDS = 31536000 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
